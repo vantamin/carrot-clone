33746151b3a924e2116eb50e95b7dd2291c816ff
vantamin <vantablack.min@gmail.com>
2021-10-19 12:43:51 +0900

    Install Firebase

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBsgFCnAACsSyxVw5V1NVlOd0SVMNYn1V8',
+  authDomain: 'carrot-clone-8268a.firebaseapp.com',
+  projectId: 'carrot-clone-8268a',
+  storageBucket: 'carrot-clone-8268a.appspot.com',
+  messagingSenderId: '896837902817',
+  appId: '1:896837902817:web:0d8d872074d683f1df72a0',
+};
+
+// eslint-disable-next-line no-unused-vars
+const app = initializeApp(firebaseConfig);
