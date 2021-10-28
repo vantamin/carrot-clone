e9207c85d9e871891653b47748149d9964284307
vantamin <vantablack.min@gmail.com>
2021-10-28 17:10:45 +0900

    feat: upload

--- src/upload.js
+++ src/upload.js
@@ -1,23 +1,35 @@
 import { collection, addDoc } from 'firebase/firestore';
-import { ref, uploadBytesResumable } from 'firebase/storage';
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import { db, storage } from './firebase';
 
 document.querySelector('#send').addEventListener('click', () => {
   const file = document.querySelector('#image').files[0];
   const storageRef = ref(storage, `images/${file.name}`);
-  uploadBytesResumable(storageRef, file);
+  const uploadTask = uploadBytesResumable(storageRef, file);
 
-  addDoc(collection(db, 'products'), {
-    title: document.querySelector('#title').value,
-    price: document.querySelector('#price').value,
-    content: document.querySelector('#content').value,
-    date: new Date(),
-  })
-    .then((result) => {
-      console.log(result);
-      window.location.href = '/index.html';
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  uploadTask.on(
+    'state_changed',
+    null,
+    (error) => {
+      console.error(error);
+    },
+    () => {
+      getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+        addDoc(collection(db, 'products'), {
+          title: document.querySelector('#title').value,
+          price: document.querySelector('#price').value,
+          content: document.querySelector('#content').value,
+          date: new Date(),
+          image: url,
+        })
+          .then((result) => {
+            console.log(result);
+            window.location.href = '/index.html';
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+    }
+  );
 });
